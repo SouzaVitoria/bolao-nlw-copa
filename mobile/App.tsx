287658b2de2454a0b0fb8b3d39365ede1580c7c8
SouzaVitoria <viihsouza161@gmail.com>
2022-11-02 16:30:32 -0300

    mobile: install google fonts and Roboto font family

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeBaseProvider, Center, Text } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from "@expo-google-fonts/roboto"
 
 import { THEME } from "./src/styles/theme"
 
 export default function App() {
+  useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
+
   return (
     <NativeBaseProvider theme={THEME}>
       <Center flex={1} bgColor="gray.900">
-        <Text fontSize={24} color="white">Hello!</Text>
+        <Text fontSize={24} color="white" fontFamily="heading">Hello!</Text>
         <StatusBar style="auto" />
       </Center>
     </NativeBaseProvider>
