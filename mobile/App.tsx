4b9f5dc52dabb536f0c19fc707a48d8a1a8a670f
SouzaVitoria <viihsouza161@gmail.com>
2022-11-02 15:18:58 -0300

    mobile: using some NativeBase components

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,23 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NativeBaseProvider } from "native-base";
+import { NativeBaseProvider, Center, Text } from "native-base";
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
+      <Center flex={1} bgColor="black">
+        <Text fontSize={24} color="white">Hello!</Text>
         <StatusBar style="auto" />
-      </View>
+      </Center>
     </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
