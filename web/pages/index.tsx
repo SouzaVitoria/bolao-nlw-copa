19326042af9476e3cc05abb30fbefe39d9c05220
SouzaVitoria <viihsouza161@gmail.com>
2022-11-02 10:26:40 -0300

    front-end: using fetch for data get in front

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,4 +1,8 @@
 export default function Home() {
+  fetch("http://localhost:3333/pools/count")
+    .then(response => response.json())
+    .then(data => console.log(data))
+
   return (
     <>
       <p>Initial structure</p>
