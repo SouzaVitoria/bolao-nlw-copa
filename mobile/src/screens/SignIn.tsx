81dff21724733cb9cda89f2af6a77a6e8ea83927
SouzaVitoria <viihsouza161@gmail.com>
2022-11-04 08:28:53 -0300

    mobile: svg import and using

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,10 +1,12 @@
-import { Center, Text } from "native-base";
+import { Center } from "native-base";
+
+import Logo from "../assets/logo.svg"
 
 export default function SignIn() {
 
   return (
     <Center flex={1} bgColor="gray.900">
-      <Text fontSize={24} color="white" fontFamily="heading">SignIn</Text>
+      <Logo width={212} height={40} />
     </Center>
   )
 }
