292847fa0eff30503bfb5b76371165eb13d2159e
SouzaVitoria <viihsouza161@gmail.com>
2022-11-04 07:21:17 -0300

    front-end: add lp styles

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -7,46 +7,63 @@ import iconCheckImg from "../assets/icon-check.svg"
 
 export default function Home(props: HomeProps) {
   return (
-    <>
+    <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center">
       <main>
         <Image
           src={logoImg}
           alt="Logo NLW Copa"
         />
-        <h1>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
-        <div>
+        <h1 className="mt-14 text-white text-5xl font-bold leading-tight">
+          Crie seu próprio bolão da copa e compartilhe entre amigos!
+        </h1>
+
+        <div className="mt-10 flex items-center gap-2">
           <Image
             src={usersAvatarExampleImg}
             alt=""
           />
-          <strong>
-            <span>+12.592</span> pessoas já estão usando
+          <strong className="text-gray-100 text-xl">
+            <span className="text-ignite-500">+12.592</span> pessoas já estão usando
           </strong>
         </div>
-        <form>
-          <input type="text" placeholder='Qual nome do seu bolão' required />
-          <button type="submit"> CRIAR MEU BOLÃO </button>
-        </form>
-        <p>Após criar seu boão, você receberá um código único que poderá usar para convidar outras pessoas 🚀 </p>
 
-        <div>
-          <div>
+        <form className="mt-10 flex gap-2">
+          <input
+            type="text"
+            className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm"
+            placeholder="Qual nome do seu bolão"
+            required
+          />
+          <button
+            type="submit"
+            className="bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm hover:bg-yellow-700"
+          > CRIAR MEU BOLÃO
+          </button>
+        </form>
+
+        <p className="mt-4 text-sm text-gray-300 leading-relaxed">
+          Após criar seu boão, você receberá um código único que poderá usar para convidar outras pessoas 🚀
+        </p>
+
+        <div className="mt-10 pt-10 border-t border-gray-600 flex items-center justify-between text-gray-100">
+          <div className="flex items-center gap-6">
             <Image
               src={iconCheckImg}
               alt=""
             />
-            <div>
-              <span> +192.847 </span>
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl"> +192.847 </span>
               <span> Bolões criados </span>
             </div>
           </div>
-          <div>
+          <div className="w-px h-14 bg-gray-600" />
+          <div className="flex items-center gap-6">
             <Image
               src={iconCheckImg}
               alt=""
             />
-            <div>
-              <span> +192.847 </span>
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl"> +192.847 </span>
               <span> Bolões criados </span>
             </div>
           </div>
@@ -56,7 +73,7 @@ export default function Home(props: HomeProps) {
         src={appPreviewImg}
         alt="Dois celulares exibindo uma prévia da aplicação móvel do NLW Copa"
       />
-    </>
+    </div>
   )
 }
 
