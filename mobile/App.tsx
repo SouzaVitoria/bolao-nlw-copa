172435b07b0db92d6d9eb622917e6a28e66eacd4
SouzaVitoria <viihsouza161@gmail.com>
2022-11-05 11:38:06 -0300

    mobile: create FindPool interface

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from "@
 import { AuthContextProvider } from "./src/contexts/AuthContext";
 
 import { Loading } from './src/components/Loading';
-import { NewPool } from './src/screens/NewPool';
+import { FindPool } from './src/screens/FindPool';
 import { THEME } from "./src/styles/theme"
 
 export default function App() {
@@ -18,7 +18,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <NewPool /> : <Loading />}
+        {fontsLoaded ? <FindPool /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
